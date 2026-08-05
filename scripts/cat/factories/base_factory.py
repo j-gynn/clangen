b350eadabd52d2f89b843eb2a7f5fc4b9064f0cc
J Gynn <48025294+j-gynn@users.noreply.github.com>
2026-08-05 19:30:52 +0100

    added catstore to base factory

--- scripts/cat/factories/base_factory.py
+++ scripts/cat/factories/base_factory.py
@@ -3,16 +3,13 @@ from typing import TYPE_CHECKING
 
 
 from scripts.cat.cats import Cat
-from scripts.cat.registry_module.store import CatStore
+from scripts.cat.registry_module.store import CatStore, cat_store
 
 
 class BaseCatFactory(ABC):
-    def __init__(self, store: CatStore):
-        self._cat_store = store
-
     def create_cat(self, **kwargs) -> Cat:
         cat = self._build_cat(**kwargs)
-        self._cat_store.add(cat)
+        cat_store.add(cat)
         return cat
 
     @abstractmethod
---
+++ scripts/cat/registry_module/store.py
@@ -0,0 +1,26 @@
+from typing import TYPE_CHECKING, Dict
+
+from scripts.cat.registry_module._query import CatQuery
+
+if TYPE_CHECKING:
+    from scripts.cat.cats import Cat
+
+
+class CatStore:
+    def __init__(self):
+        self._cats: Dict[str, "Cat"] = {}
+
+    def add(self, cat: "Cat"):
+        self._cats[cat.ID] = cat
+
+    def remove(self, cat_id: str):
+        self._cats.pop(cat_id)
+
+    def get(self, cat_id) -> "Cat":
+        return self._cats[cat_id]
+
+    def clear(self):
+        self._cats = {}
+
+
+cat_store: CatStore = CatStore()
